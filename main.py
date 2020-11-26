844877c2d8249a9a803d74d6df0d3fe6bffc18f6
rafalstaroszczyk <74345652+rafalstaroszczyk@users.noreply.github.com>
2020-11-26 12:40:18 +0100

    uogolnienie obliczen i rozdzielenie na funkcje

--- main.py
+++ main.py
@@ -1,27 +1,60 @@
 import numpy as np
 
-def epsilon(t, phi):
+
+def epsilon(phi, alfa, z):  # przyspieszenie pionowe
     global a, f, g, l, dt
-    return 3 / 2 * (4 * np.pi ** 2 * a * f ** 2 * np.sin(2 * np.pi * f * t) - g) / l * np.sin(phi)
+    acc = np.array((alfa, 4 * np.pi ** 2 * f ** 2 * z - g))  # wektor przyspieszenia: w lewo, w gore
+    return 3 / 2 * 1 / l * np.dot(acc, np.array((-np.cos(phi), np.sin(phi))))
 
 
-dt = 0.0001
-t = 10
-n = int(t / dt)
-phi0 = 0.99 * np.pi
-omega0 = 0
-a = 0.01
-l = 1
-g = 10
-f = 100
-data = np.zeros((n, 5))  # data: i, t, phi, omega, epsilon
-data[0, 2] = phi0
-data[0, 3] = omega0
-data[0, 4] = -3 / 2 * g / l * np.sin(phi0)
-for i in range(n-1):
-    data[i + 1, 0:4] = data[i, 0:4] + np.array((1, dt, data[i, 3] * dt, data[i, 4] * dt))
-    data[i + 1,  4] = epsilon(data[i, 1], data[i, 2])
+def alfa():  # przyspieszenie poziome
+    return 0
+
+
+def simulate():
+    global dt, t, n, phi0, omega0, x0, v0, a, l, g, f, data
+    # ustawianie wartosci poczatkowych
+    data[0, 2] = phi0
+    data[0, 3] = x0
+    data[0, 4] = omega0
+    data[0, 5] = v0
+    data[0, 6] = -3 / 2 * g / l * np.sin(phi0)
+    data[0, 7] = alfa()
+    for i in range(n - 1):
+        data[i + 1, 0:6] = data[i, 0:6] + np.array(
+            (1, dt, data[i, 4] * dt, data[i, 5] * dt, data[i, 6] * dt, data[i, 7]))
+        data[i + 1, 6] = epsilon(data[i, 2], data[i, 7], data[i, 8])
+        data[i + 1, 7] = alfa()
+        data[i + 1, 8] = a * np.sin(2 * np.pi * f * data[i, 1])
+
+
+dt = 0.001  # skok probkowania
+t = 1  # czas symulacji
+n = int(t / dt)  # ilosc probek
+phi0 = 0.99 * np.pi  # kat poczatkowy
+omega0 = 0  # predkosc katowa poczatkowa
+x0 = 0  # polozenie poczatkowe poziome pktu obrotu
+v0 = 0  # predkosc poczatkowa pozioma pktu obrotu
+a = 0.01  # amplituda drgan
+l = 1  # dlugosc wahadla
+g = 10  # przysp. grawitacyjne
+f = 100  # czestotliwosc drgan
+
+data = np.zeros((n, 9))
+"""
+data[:, 0] = i - index
+data[:, 1] = t - czas
+data[:, 2] = phi - kat od -OZ w lewo
+data[:, 3] = x - pol. poziome pktu obrotu
+data[:, 4] = omega - pred. katowa
+data[:, 5] = v - predkosc pozioma pktu obrotu
+data[:, 6] = epsilon - przysp. katowe
+data[:, 7] = alfa - przysp. poziome pkt. obrotu
+data[:, 8] = z - pol. pionowe pktu obrotu
+"""
+simulate()
 offset = np.zeros((1, n))
 offset[:] = np.pi
-data[:, 2] = offset - data[:, 2]
-np.savetxt("data", data, fmt="%f")
+data[:, 2] = offset - data[:, 2]  # kat od OZ w prawo
+np.savetxt("data", data, fmt="%10.5f",
+           header='%8s%11s%11s%11s%11s%11s%11s%11s%11s' % ('i', 't', 'phi', 'x', 'omega', 'v', 'epsilon', 'alfa', 'z'))
